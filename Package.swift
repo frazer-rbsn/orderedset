85ded1873d6b8e705f521282850e1799c5a2e743
Frazer Robinson <frobinson05@qub.ac.uk>
2025-06-22 12:27:27 +0100

    Spaces -> Tabs

--- Package.swift
+++ Package.swift
@@ -2,23 +2,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "OrderedSet",
-  platforms: [
-    .iOS(.v12),
-    .macOS(.v10_13)
-  ],
-  products: [
-    .library(
-      name: "OrderedSet",
-      targets: ["OrderedSet"])
-  ],
-  targets: [
-    .target(
-      name: "OrderedSet",
-      path: "Sources/"),
-    .testTarget(
-      name: "OrderedSetTests",
-      dependencies: ["OrderedSet"],
-      path: "Tests/")
-  ]
+	name: "OrderedSet",
+	platforms: [
+		.iOS(.v12),
+		.macOS(.v10_13)
+	],
+	products: [
+		.library(
+			name: "OrderedSet",
+			targets: ["OrderedSet"])
+	],
+	targets: [
+		.target(
+			name: "OrderedSet",
+			path: "Sources/"),
+		.testTarget(
+			name: "OrderedSetTests",
+			dependencies: ["OrderedSet"],
+			path: "Tests/")
+	]
 )
